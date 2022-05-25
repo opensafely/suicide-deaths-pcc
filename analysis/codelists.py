8b9df4334e77f9d964c840ee31851c635f5b36ef
Milan Wiedemann <milan.wiedemann@gmail.com>
2022-05-25 15:43:26 +0100

    add ethnicity and sex variables

---
+++ analysis/codelists.py
@@ -0,0 +1,8 @@
+from cohortextractor import codelist_from_csv
+
+ethnicity6_codes = codelist_from_csv(
+    "codelists/opensafely-ethnicity.csv",
+    system="ctv3",
+    column="Code",
+    category_column="Grouping_6",
+)
--- analysis/study_definition.py
+++ analysis/study_definition.py
@@ -1,5 +1,5 @@
 from cohortextractor import StudyDefinition, patients, codelist, codelist_from_csv  # NOQA
-
+from codelists import ethnicity6_codes
 
 study = StudyDefinition(
     default_expectations={
@@ -13,10 +13,12 @@ study = StudyDefinition(
         age > 16
         """
     ),
+    # Define registered variable
     registered=patients.registered_as_of(
         "2020-03-31",
         return_expectations={"incidence": 0.9},
     ),
+    # Define age variable
     age=patients.age_as_of(
         "2020-03-31",
         return_expectations={
@@ -24,5 +26,46 @@ study = StudyDefinition(
             "int": {"distribution": "population_ages"},
         },
     ),
-
+    # Define sex variable
+    sex=patients.sex(
+        return_expectations={
+            "rate": "universal",
+            "category": {"ratios": {"M": 0.49, "F": 0.51}},
+        }
+    ),
+    # Define ethnicity variable
+    ethnicity=patients.categorised_as(
+        {
+            "Unknown": "DEFAULT",
+            "White": "eth='1'",
+            "Mixed": "eth='2'",
+            "Asian": "eth='3'",
+            "Black": "eth='4'",
+            "Other": "eth='5'",
+        },
+        eth=patients.with_these_clinical_events(
+            ethnicity6_codes,
+            returning="category",
+            find_last_match_in_period=True,
+            include_date_of_match=False,
+            return_expectations={
+                "category": {
+                    "ratios": {"1": 0.2, "2": 0.2, "3": 0.2, "4": 0.2, "5": 0.2}
+                },
+                "incidence": 0.75,
+            },
+        ),
+        return_expectations={
+            "category": {
+                "ratios": {
+                    "White": 0.2,
+                    "Mixed": 0.2,
+                    "Asian": 0.2,
+                    "Black": 0.2,
+                    "Other": 0.2,
+                }
+            },
+            "incidence": 0.8,
+        },
+    ),
 )
